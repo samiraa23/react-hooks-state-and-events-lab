6cbada05c8c6ca5464eae633d4c3fdcb7837c862
samiraa23 <samiraroble02@gmail.com>
2024-07-04 10:51:36 +0300

    submit react use state

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  const handCartToggle = () => {
+    setIsInCart(!isInCart);
+  };
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart": ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handCartToggle}className="add">{isInCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
+
 
 export default Item;
